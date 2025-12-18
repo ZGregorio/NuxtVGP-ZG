b05dafa4969118a4584593372df90facfe057fa5
ZGregorio <zejjgregg@gmail.com>
2025-12-18 18:38:15 +0800

    Updated Filter.ts
    Quick code adjustments

--- app/composables/useFilter.ts
+++ app/composables/useFilter.ts
@@ -3,10 +3,11 @@ export function useFilter(
     selectedYear: Ref<number | null>
 ) {
      const filteredLaunches = computed(() => {
-    if (!selectedYear.value) return launches.value
+        if (!selectedYear.value) 
+            return launches.value
 
-    return launches.value.filter(
-      launch => new Date(launch.launch_date_utc).getFullYear() === selectedYear.value
+        return launches.value.filter(
+        launch => new Date(launch.launch_date_utc).getFullYear() === selectedYear.value
     )
   })
 return {filteredLaunches}
